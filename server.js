a189da75f2fbbdc8f8297e95c4e197ce5be3c981
Tim Malstead <timmmalstead@gmail.com>
2020-03-12 09:37:23 -0700

    preps for push

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const app = express()
 require("dotenv").config()
 
-const PORT = process.env.S_PORT || 8000
+const PORT = process.env.PORT || 8000
 
 const adminRouter = require("./controller/admin.js")
 const dataRouter = require("./controller/data.js")
